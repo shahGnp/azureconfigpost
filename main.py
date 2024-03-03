c35e4783325dfc73686204673c54ef55c346f006
shahGnp <gaurav.shah@sagarmatha.edu.np>
2024-03-03 21:10:11 +0545

    cors error fix

--- main.py
+++ main.py
@@ -3,12 +3,21 @@ from typing import Union
 from fastapi import FastAPI
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 import os
 from random import randint
 import uuid
 from aimodel_post import detectImage as detectImagePost
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 IMAGEDIR='.'
 
